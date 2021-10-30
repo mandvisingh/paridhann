1a6e10fc4ef54d95fb5f5d907c9ef9b23bbb1078
mandvisingh <mandvisingh64@gmail.com>
2021-10-30 17:57:17 +0530

    fix: header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import Homepage from './pages/homepage/Hompepage';
 import {Route, Switch} from 'react-router-dom';
 import ShopPage from './pages/shopPage/shopPage';
+import Header from './component/header/header';
 
 function App() {
   return (
     <div className="App" style={{
       'font-family': 'Open Sans Condensed'
     }}>
+      <Header/>
       <Switch>
         <Route exact path='/' component={Homepage}></Route>
-        <Route  path='/shop' component={ShopPage}></Route>
+        <Route path='/shop' component={ShopPage}></Route>
       </Switch>
     </div>
   );
