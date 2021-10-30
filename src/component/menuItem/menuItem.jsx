966650eb688f5c5ee325ef89962474f389230afe
mandvisingh <mandvisingh64@gmail.com>
2021-10-30 15:40:36 +0530

    feat: styling transitions

--- src/component/menuItem/menuItem.jsx
+++ src/component/menuItem/menuItem.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import './styles.scss';
 
 const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`${size} menu-item`} style={{
+    <div className={`${size} menu-item`} >
+    <div 
+    className="background-image"
+    style={{
         background: `url(${imageUrl})`
-    }}>
+    }}>  </div>
     <div className="content">
         <h2  className="title">{title}</h2>
         <span  className="subtitle">SHOP NOW</span>
